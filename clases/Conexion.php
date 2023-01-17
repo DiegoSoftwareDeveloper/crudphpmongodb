6eb77ac16f5977fe25eb49da4421691bb6c3ff84
DiegoSoftwareDeveloper <diegobarreto702@gmail.com>
2023-01-16 21:17:44 -0500

    feat: Ajuste total de conexiones

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -1,11 +1,11 @@
 <?php
-
+use MongoDB\Client as Mongo;
 require_once $_SERVER['DOCUMENT_ROOT'] . "/crudphpmongo/vendor/autoload.php";
 
 class conexion {
     public function conectar(){
         try {
-            $servercon = "localhost";       
+            $servercon = "127.0.0.1";       
             $port = "27017";
             $user = "mongoadmin";
             $password = "123456";
@@ -18,12 +18,16 @@ class conexion {
                                 $port . "/" .
                                 $databasecon;
 
-                                $client = new MongoDB\Client($canConnection);
+                                $client = new Mongo($canConnection);
+                                //$client = new MongoDB\Driver\Manager('mongodb://localhost:27017');
                                 return $client->selectDatabase($databasecon);
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
     }
 }
+
+$objeto = new Conexion();
+var_dump($objeto->conectar());
 
 ?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5618e1f8b4864b3cf4af38cd9ceb3eb7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5618e1f8b4864b3cf4af38cd9ceb3eb7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5618e1f8b4864b3cf4af38cd9ceb3eb7', 'loadClassLoader'));
